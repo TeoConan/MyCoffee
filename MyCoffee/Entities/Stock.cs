4fef6e4d8745e3f1da00fc694d92cf4b7c4eb026
alexPouivet <you@example.com>
2019-10-29 16:49:29 +0100

    create stock entity

---
+++ MyCoffee/Entities/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCoffee.Entities
+{
+    public class Stock
+    {
+        public int Id { get; }
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }
+
+    }
+}
