c47b44ecab859ad65fe805e49c7ba84d21295b2b
Eva Saintier <eva.saintier@my-digital-school.org>
2019-10-29 16:49:33 +0100

    customer

--- MyCoffee/Entities/Customer.cs
+++ MyCoffee/Entities/Customer.cs
@@ -6,5 +6,14 @@ namespace MyCoffee.Entities
 {
     public class Customer
     {
+        public Customer()
+        {
+            //Orders = new List<Order>();
+        }
+
+        public int CustomerId { get; }
+        public string Name { get; set; }
+        public float Balance { get; set; }
+        //public List<Order> Orders { get; set; }
     }
 }
