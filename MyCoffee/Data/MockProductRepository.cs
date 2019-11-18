c5f924f050fd75194d3f4bd473f3897f0f65bb6a
Markonius <markonius.dev@gmail.com>
2019-11-18 15:24:36 +0100

    :sparkles: Fill Product mock

--- MyCoffee/Data/MockProductRepository.cs
+++ MyCoffee/Data/MockProductRepository.cs
@@ -7,8 +7,35 @@ namespace MyCoffee.Data
 {
     public class MockProductRepository : IProductsRepository
     {
+        private List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, CategoryId = 2, Name = "Panini Chelou", Description = "Contenu étrange de sucré-salé", Price = 5 },
+            new Product { Id = 2, CategoryId = 2, Name = "Panini Mieux", Description = "Peu import, c'est meilleur", Price = 5 },
+            new Product { Id = 3, CategoryId = 1, Name = "Croissant", Description = "C'est juste un croissant", Price = 1.05F }
+        };
+
         public MockProductRepository()
         {
+        }
+
+        public IEnumerable<Product> getAllProducts()
+        {
+            return null;
+        }
+
+        public IEnumerable<Product> getProductsByCategory(int categoryId)
+        {
+            return null;
+        }
+
+        public Product getProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product getProductByid(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
