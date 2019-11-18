9c3715397a685148867af1f8f82e42fefb4864e3
Markonius <markonius.dev@gmail.com>
2019-11-18 15:07:41 +0100

    :sparkles: Create product repository and mock

---
+++ MyCoffee/Data/IProductsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MyCoffee.Entities;
+
+namespace MyCoffee.Data
+{
+    public interface IProductsRepository
+    {
+		IEnumerable<Product> getAllProducts();
+
+        Product getProductByName(string name);
+
+		IEnumerable<Product> getProductsByCategory(int categoryId);
+
+		Product getProductByid(int id);
+    }
+}
---
+++ MyCoffee/Data/MockProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using MyCoffee.Entities;
+using System.Linq;
+
+namespace MyCoffee.Data
+{
+    public class MockProductRepository : IProductsRepository
+    {
+        public MockProductRepository()
+        {
+        }
+    }
+}
