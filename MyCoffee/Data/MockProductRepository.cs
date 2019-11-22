7bc3865e5027d3cb3abfd5278b24845f3f660a84
Téo Conan <teo.conan.tirault@gmail.com>
2019-11-22 12:27:21 +0100

    Add test mock

--- MyCoffee/Data/MockProductRepository.cs
+++ MyCoffee/Data/MockProductRepository.cs
@@ -11,7 +11,15 @@ namespace MyCoffee.Data
         {
             new Product { Id = 1, CategoryId = 2, Name = "Panini Chelou", Description = "Contenu étrange de sucré-salé", Price = 5 },
             new Product { Id = 2, CategoryId = 2, Name = "Panini Mieux", Description = "Peu import, c'est meilleur", Price = 5 },
-            new Product { Id = 3, CategoryId = 1, Name = "Croissant", Description = "C'est juste un croissant", Price = 1.05F }
+            new Product { Id = 3, CategoryId = 1, Name = "Croissant", Description = "C'est juste un croissant", Price = 1.05F },
+            new Product { Id = 4, CategoryId = 5, Name = "Brownie", Description = "erverv", Price = 0.99F },
+            new Product { Id = 5, CategoryId = 5, Name = "Muffin", Description = "ervzrtb", Price = 1F },
+            new Product { Id = 6, CategoryId = 6, Name = "PastaBox Bolo", Description = "dsdfgfngh,fj;gk", Price = 6.60F },
+            new Product { Id = 7, CategoryId = 6, Name = "Pizza 4 fromage", Description = "dfbs", Price = 7.90F },
+            new Product { Id = 8, CategoryId = 4, Name = "Ice Tea", Description = "qr", Price = 4.55F },
+            new Product { Id = 9, CategoryId = 3, Name = "Salade Piedmontaise", Description = "qrdqdb", Price = 8.45F },
+            new Product { Id = 10, CategoryId = 5, Name = "Snicker", Description = "qdvqrv", Price = 1.45F },
+            new Product { Id = 11, CategoryId = 0, Name = "Café", Description = "qrgrqbn", Price = 99F }
         };
 
         public MockProductRepository()
