7bdd21b8aa8101389a8fa04eb4f269b7d5b7de6a
Eva Saintier <eva.saintier@my-digital-school.org>
2019-11-22 10:13:39 +0100

    product categories

---
+++ MyCoffee/Data/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyCoffee.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCoffee.Data
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> Get();
+    }
+}
---
+++ MyCoffee/Data/MockCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyCoffee.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCoffee.Data
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Get()
+        {
+            yield return new Category { CategoryId = 0, Value = "Hot drinks" };
+            yield return new Category { CategoryId = 1, Value = "Viennoiseries" };
+            yield return new Category { CategoryId = 2, Value = "Sandwiches" };
+            yield return new Category { CategoryId = 3, Value = "Salads" };
+            yield return new Category { CategoryId = 4, Value = "Drinks" };
+            yield return new Category { CategoryId = 5, Value = "Snacks" };
+            yield return new Category { CategoryId = 6, Value = "Lunch boxes" };
+        }
+    }
+}
