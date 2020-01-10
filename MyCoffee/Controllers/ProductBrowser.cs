60f9c9f2e3742ddb64f10dfd63cb949d848e0e7f
Markonius <markonius.dev@gmail.com>
2020-01-10 14:34:13 +0100

    :sparkles: Create a new class

---
+++ MyCoffee/Controllers/ProductBrowser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MyCoffee.Controllers
+{
+    public class ProductBrowser : MyCoffeeConsole
+    {
+        public ProductBrowser()
+        {
+        }
+
+        protected override void DecisionTree(string Input, bool DisplayMenu)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
