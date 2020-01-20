664f7a93e576feb5e3b01793efae96b85291efcd
Téo Conan <teo.conan.tirault@gmail.com>
2020-01-20 09:37:48 +0100

    Add CustomerRepository and refactorize

---
+++ MyCoffee/Data/CustomerRepository.cs
@@ -0,0 +1,39 @@
+﻿using MyCoffee.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCoffee.Data
+{
+    class CustomerRepository
+    {
+        public bool AddCustomer(Customer customer)
+        {
+            using (var dboContext = new MCDBContext())
+            {
+                dboContext.Customer.Add(customer);
+                return (dboContext.SaveChanges() > 0);
+            }
+
+            return false;
+        }
+
+        public List<Customer> GetAllCustomer()
+        {
+            List<Customer> listCustomers = new List<Customer>();
+
+            using (var dboContext = new MCDBContext())
+            {
+                var DbList = dboContext.Customer;
+
+                foreach (Customer Customer in DbList)
+                {
+                    listCustomers.Add(Customer);
+                }
+            }
+
+            return listCustomers;
+
+        }
+    }
+}
--- MyCoffee/Entities/Customer.cs
+++ MyCoffee/Entities/Customer.cs
@@ -20,34 +20,5 @@ namespace MyCoffee.Entities
         public int TimeUpdate { set; get; }
         public int TimeDelete { set; get; }
 
-
-        public static bool AddCustomer(Customer customer)
-        {
-            using (var dboContext = new MCDBContext())
-            {
-                dboContext.Customer.Add(customer);
-                return (dboContext.SaveChanges() > 0);
-            }
-
-            return false;
-        }
-
-        public static List<Customer> GetAllCustomer()
-        {
-            List<Customer> listCustomers = new List<Customer>();
-
-            using (var dboContext = new MCDBContext())
-            {
-                var DbList = dboContext.Customer;
-
-                foreach (Customer Customer in DbList)
-                {
-                    listCustomers.Add(Customer);
-                }
-            }
-
-            return listCustomers;
-
-        }
     }
 }
